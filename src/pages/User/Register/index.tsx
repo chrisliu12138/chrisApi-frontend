4f482699f44444c7a388b2352cfcab013aabdf8f
chrisliu12138 <liuhao.hi@qq.com>
2023-09-09 15:14:38 +0800

    Solve the duplicate bugs of the online and offline buttons, etc.

---
+++ src/pages/User/Register/index.tsx
@@ -0,0 +1,154 @@
+import Footer from '@/components/Footer';
+
+import { userRegisterUsingPOST } from '@/services/chrisApi-backend/userController';
+import { Link } from '@@/exports';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { LoginForm, ProFormText } from '@ant-design/pro-components';
+import { useEmotionCss } from '@ant-design/use-emotion-css';
+import { Helmet, history } from '@umijs/max';
+import { message, Tabs } from 'antd';
+import React, { useState } from 'react';
+import Settings from '../../../../config/defaultSettings';
+
+const Register: React.FC = () => {
+  const [type, setType] = useState<string>('account');
+  const containerClassName = useEmotionCss(() => {
+    return {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      overflow: 'auto',
+      backgroundImage:
+        "url('https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/V-_oS6r-i7wAAAAAAAAAAAAAFl94AQBr')",
+      backgroundSize: '100% 100%',
+    };
+  });
+
+  const handleSubmit = async (values: API.UserRegisterRequest) => {
+    try {
+      // 注册
+      const res = await userRegisterUsingPOST(values);
+      if (res.code === 0) {
+        const defaultRegisterSuccessMessage = 'Registration Success！';
+        message.success(defaultRegisterSuccessMessage);
+        const urlParams = new URL(window.location.href).searchParams;
+        history.push(urlParams.get('redirect') || '/user/login');
+        return;
+      } else {
+        message.error(res.message);
+      }
+    } catch (error) {
+      const defaultLoginFailureMessage = 'Registration Failed, Please Retry！';
+      console.log(error);
+      message.error(defaultLoginFailureMessage);
+    }
+  };
+  return (
+    <div className={containerClassName}>
+      <Helmet>
+        <title>
+          {'Register'}- {Settings.title}
+        </title>
+      </Helmet>
+      <div
+        style={{
+          flex: '1',
+          padding: '32px 0',
+        }}
+      >
+        <LoginForm
+          contentStyle={{
+            minWidth: 280,
+            maxWidth: '75vw',
+          }}
+          submitter={{
+            searchConfig: {
+              submitText: 'Register',
+            },
+          }}
+          logo={<img alt="logo" src="/logo.svg" />}
+          title="API OPEN PLATFORM"
+          subTitle={'Open and Secure API Calling Platform Based on Spring Cloud Gateway'}
+          onFinish={async (values) => {
+            await handleSubmit(values as API.UserRegisterRequest);
+          }}
+        >
+          <Tabs
+            activeKey={type}
+            onChange={setType}
+            centered
+            items={[
+              {
+                key: 'account',
+                label: 'Register',
+              },
+            ]}
+          />
+          {type === 'account' && (
+            <>
+              <ProFormText
+                name="userAccount"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <UserOutlined />,
+                }}
+                placeholder={'Please enter your username'}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Username is required！',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="userPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined />,
+                }}
+                placeholder={'Please enter your password'}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Password is required！',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="checkPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined />,
+                }}
+                placeholder={'Please enter your password again'}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Password is required！',
+                  },
+                ]}
+              />
+            </>
+          )}
+          <div
+            style={{
+              marginBottom: 24,
+            }}
+          >
+            <Link
+              style={{
+                float: 'right',
+              }}
+              to="/user/login"
+            >
+              Already have an account? Click to login
+            </Link>
+            <br />
+          </div>
+        </LoginForm>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+export default Register;
